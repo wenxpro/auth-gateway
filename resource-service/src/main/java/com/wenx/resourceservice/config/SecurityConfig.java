f74995b4c9d3dfc3d880391a6576204af23927b5
Chaimouren <sxycwx2013@163.com>
2019-12-22 00:59:52 +0800

    add:增加说明

--- resource-service/src/main/java/com/wenx/resourceservice/config/SecurityConfig.java
+++ resource-service/src/main/java/com/wenx/resourceservice/config/SecurityConfig.java
@@ -1,20 +1,20 @@
 package com.wenx.resourceservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http
-                .authorizeExchange()
+        http.authorizeExchange()
+                .pathMatchers("/hello/").permitAll()
                 .pathMatchers("/**").authenticated()
-                .anyExchange()
-                .authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
+                .anyExchange().authenticated()
+                .and().httpBasic();
+        http.oauth2ResourceServer().jwt();
         return http.build();
     }
 }
